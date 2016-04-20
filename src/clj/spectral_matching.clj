9a86cdc5cfe10cace52108d2a5ce362c2bb972a0
dolbydc <donevan.dolby@base2s.com>
2016-04-20 15:03:39 -0700

    Migrate to core.reducers library. (#1)
    * Migrate to core.reducers library.
    
    * Move to r/fold and r/map
    
    * more Moves to r/fold and r/map

--- src/clj/spectral_matching.clj
+++ src/clj/spectral_matching.clj
@@ -1,5 +1,7 @@
 (ns spectral-matching
-  [:require [clojure.pprint]])
+  (:gen-class)
+  [:require [clojure.pprint]
+            [clojure.core.reducers :as r]])
 
 (defn- reduce-fn
   [result-map line-data]
@@ -9,19 +11,20 @@
 (defn- get-sample-data
   [sample-filename]
   (with-open [rdr (clojure.java.io/reader sample-filename)]
-    (reduce reduce-fn {} (line-seq rdr))))
+    (r/reduce reduce-fn {} (line-seq rdr))))
 
-(defn- known-data-reduce-fn
-  [sample-folder result-map index]
-  (let [sample-name (str "sample_" index)
-        sample-filename (str sample-folder "/" sample-name ".spr")
-        sample-data (get-sample-data sample-filename)]
-    (assoc result-map sample-name sample-data)))
+(defn- create-known-data-fn
+  [sample-folder]
+  (fn [index]
+    (let [sample-name (str "sample_" index)
+          sample-filename (str sample-folder "/" sample-name ".spr")
+          sample-data (get-sample-data sample-filename)]
+      {sample-name sample-data})))
 
 (defn- get-known-data
   [sample-folder range-begin range-end]
-  (reduce (partial known-data-reduce-fn sample-folder)
-          {} (range range-begin range-end)))
+  (let [map-fn (create-known-data-fn sample-folder)]
+    (r/fold merge (r/map map-fn (range range-begin range-end)))))
 
 (defn- percentage-fn
   [sample known]
@@ -31,35 +34,37 @@
       (/ sample known))
     (catch java.lang.ArithmeticException ae 1)))
 
-(defn- compare-sample-fn
-  [sample-data known-data sum lookup-key]
-  (let [sample (get sample-data lookup-key)
-        known (get known-data lookup-key)
-        percentage (percentage-fn sample known)]
-    (+ sum percentage)))
+(defn- create-compare-sample-fn
+  [sample-data]
+  (fn
+    ([k v]
+      (let [sample-value (get sample-data k)]
+        (percentage-fn sample-value v)))))
 
 (defn- compare-sample-to-known
   [sample-data known-data]
-  (let [compare-sum (reduce (partial compare-sample-fn sample-data known-data) 0 (keys known-data))]
-    (/ compare-sum (count (keys known-data)))))
+  (-> (r/fold + (r/map (create-compare-sample-fn sample-data) known-data))
+      (#(if (seq sample-data)
+          (/ % (count sample-data))
+          0.0))))
 
-(defn- compare-reduce-fn
-  [sample-data results known-data]
-  (let [[known-sample data] known-data
-        compare-data (compare-sample-to-known sample-data data)]
-    (cond
-      (empty? results)
-        [known-sample compare-data]
-      (> compare-data (second results))
-        [known-sample compare-data]
-      :else
-        results)))
-      
+(defn- create-compare-reduce-fn
+  [sample-data]
+  (fn
+    ([] [])
+    ([results known-sample data]
+      (let [compare-data (compare-sample-to-known sample-data data)]
+        (cond
+          (empty? results)
+            [known-sample compare-data]
+          (> compare-data (second results))
+            [known-sample compare-data]
+          :else
+            results)))))
 
 (defn- compare-sample-to-knowns
   [sample-data known-datas]
-  (reduce (partial compare-reduce-fn sample-data) [] known-datas)
-)
+  (r/reduce (create-compare-reduce-fn sample-data) known-datas))
 
 (defn -main [& args]
   ""
@@ -68,7 +73,5 @@
         range-end (Integer/parseInt range-end)
         known-datas (get-known-data sample-folder range-begin range-end)
         sample-data (get-sample-data (str sample-folder "/sample.spr"))
-        sample-compares (compare-sample-to-knowns sample-data known-datas)
-        ]
-    (clojure.pprint/pprint sample-compares)
-))
+        sample-compares (compare-sample-to-knowns sample-data known-datas)]
+    (clojure.pprint/pprint sample-compares)))
